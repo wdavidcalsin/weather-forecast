6ff824f46aeeb3f723e9003496510a6f6a1aceac
WDavid Calsin <david2c142@gmail.com>
2023-04-03 22:35:52 -0500

    styled: server for performance

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,6 +1,7 @@
-import express, { type Request, type Response } from "express";
 import * as dotenv from "dotenv";
+import express, { type Request, type Response } from "express";
 import fetch from "node-fetch";
+
 import { IWeather } from "./types/weather-type";
 
 dotenv.config();
