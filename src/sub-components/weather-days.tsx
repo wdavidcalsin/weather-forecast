17307570f58bc2e80b1e27023a92d5b369a9690e
WDavid Calsin <david2c142@gmail.com>
2023-04-05 01:39:58 -0500

    feat: add date in weather days

--- src/sub-components/weather-days.tsx
+++ src/sub-components/weather-days.tsx
@@ -11,13 +11,14 @@ const WeatherDays = () => {
       {fetchWeatherData.days.map((day, index) => (
         <div
           key={index}
-          className="w-16 h-16 rounded-3xl bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-white/20 via-white/10 to-white/5"
+          className="w-16 h-16 rounded-3xl bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-white/20 via-white/10 transition delay-75 to-white/5 hover:scale-105 cursor-pointer"
         >
-          <div className="flex justify-center flex-col items-center gap-4">
+          <div className="flex flex-col gap-1 h-full justify-center items-center">
             <img
               className=" w-7 h-7"
               src={conditionImage[day.conditions].image}
             />
+            <h5 className="text-[10px] text-gray-400">{day.datetime}</h5>
           </div>
         </div>
       ))}
