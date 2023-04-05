086044028725f3a350057785612c40a180574d14
WDavid Calsin <david2c142@gmail.com>
2023-04-04 23:56:22 -0500

    feat: add condition days in weather content

--- src/components/weather/weather.tsx
+++ src/components/weather/weather.tsx
@@ -10,7 +10,7 @@ const Weather = () => {
 
   return (
     <div
-      className="border border-white/10 p-3 rounded-xl h-full w-[27rem]"
+      className="border border-white/10 py-3 px-5 rounded-xl h-full w-[27rem]"
       style={{ background: "#171717" }}
     >
       {isLoading ? (
@@ -29,9 +29,24 @@ const Weather = () => {
               }
             />
           </div>
-          <div className="text-slate-50">
+          {/* <div className="text-slate-50">
             <p>Latitude: {weatherData.latitude} </p>
             <p>Longitude: {weatherData.longitude} </p>
+          </div> */}
+          <div className="flex gap-4 flex-wrap">
+            {fetchWeatherData.days.map((day, index) => (
+              <div
+                key={index}
+                className="w-16 h-16 rounded-3xl bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-white/20 via-white/10 to-white/5"
+              >
+                <div className="flex justify-center flex-col items-center gap-4">
+                  <img
+                    className=" w-7 h-7"
+                    src={conditionImage[day.conditions].image}
+                  />
+                </div>
+              </div>
+            ))}
           </div>
         </>
       )}
