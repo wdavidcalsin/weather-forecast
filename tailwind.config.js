816225fe68099b70b52bdaf1f5cc4aebd336fb4d
WDavid Calsin <david2c142@gmail.com>
2023-04-05 23:51:46 -0500

    feat: add container queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import containerQueries from "@tailwindcss/container-queries";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [containerQueries],
 };
