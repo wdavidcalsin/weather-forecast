fb8f1ad792aa6b847219c1d7ded594b95b37450f
WDavid Calsin <david2c142@gmail.com>
2023-04-07 01:21:09 -0500

    update: url weather in store constants

--- src/constant/store-constant.ts
+++ src/constant/store-constant.ts
@@ -1,3 +1,4 @@
-export const FETCH_WEATHER_URL = "http://localhost:3000/api/weather";
+export const FETCH_WEATHER_URL =
+  "https://weather-server-lake.vercel.app/api/weather";
 export const GET_WEATHER = "weather";
 export const GET_MODAL = "modal";
