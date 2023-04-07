8deef4e23fd6931863f51f691f6e11dde82aadeb
WDavid Calsin <david2c142@gmail.com>
2023-04-06 21:48:18 -0500

    build: add tailwindcss scrollbar in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 import containerQueries from "@tailwindcss/container-queries";
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [containerQueries],
+  plugins: [containerQueries, tailwindScrollbarHide],
 };
